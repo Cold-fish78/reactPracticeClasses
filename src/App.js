e7505857f5d787a87f2887f4ac6d691162d7f186
Cold-fish78 <pubg69086@gmail.com>
2022-06-12 21:31:48 +0530

    created expanding cards using react hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion  from "./components/Accordion";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const items = [
+    {
+        title : 'what is React ?',
+        content : 'React is a front end javascript framework'
+    },
+    {
+        title : 'why use React ?',
+        content : 'React is good'
+    },
+    {
+        title : 'who are you  ?',
+        content : 'i am no one'
+    }
+
+]
+
+
+export default () =>{
+    return (
+        <div>
+            <Accordion items ={items} />
+        </div>
+    )
+
 }
-
-export default App;
---
+++ src/components/Accordion.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+
+const Accordion = ({ items }) => {
+
+     const [activeIndex, setActiveIndex] = useState(null);
+
+    const onTitleClick = (index) =>{
+         console.log('this is index',index);
+        setActiveIndex(index);
+        console.log( "this is active index" +activeIndex);
+     }
+
+
+
+    const renderedItems = items.map((item, index) => {
+
+      
+        const active = index === activeIndex ? 'active' : "";
+
+        return <React.Fragment key={item.title}>
+
+            <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+                <i className="dropdown icon"></i>
+                {item.title}
+            </div>
+            <div className={`content ${active}`}>
+                <p>{item.content} </p>
+
+            </div>
+        </React.Fragment>
+    })
+
+    return <div className="ui styled accordion ">
+        <h1> {renderedItems} </h1>
+    </div>
+}
+
+
+export default Accordion;
